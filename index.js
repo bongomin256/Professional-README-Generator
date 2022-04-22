6a12abf1955239b122c1f7da44b080ba10aba838
Godfrey Bongomin <bongomin@uw.edu>
2022-04-22 11:41:45 -0700

    Adding a writeToFile function

--- index.js
+++ index.js
@@ -81,7 +81,11 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Successfully created readme.md!")
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
