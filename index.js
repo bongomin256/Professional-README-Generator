170765675921838e8ebfce6132acf3f4736aace4
Godfrey Bongomin <bongomin@uw.edu>
2022-04-22 11:44:16 -0700

    Adding init function in the index.js file

--- index.js
+++ index.js
@@ -88,7 +88,11 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then(function (data) {
+    writeToFile("README.md", markDown(data));
+  });
+}
 
 // Function call to initialize app
 init();
