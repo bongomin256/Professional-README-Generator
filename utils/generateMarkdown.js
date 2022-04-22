7a757c8f1734017cc6d2ee766e70b3b3e872a00b
Godfrey Bongomin <bongomin@uw.edu>
2022-04-22 11:31:31 -0700

    Adding generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -56,7 +56,40 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## Table of content
+    
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributiing)
+  - [Test](#test)
+  - [Questions](#questions)
+  - [Screenshots](#screenshots)
+  - [Links](#links)
+  
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ${renderLicenseSection(data.license)}
+  ## Contribution
+  ${data.contributing}
+  ## Test
+  ${data.test}
+  ## Questions
+  This is the link to my [Github profile](https://github.com/${data.github}).
+  Incase of any question[s] or suggestion[s] concerning this appication, please contact me through this email:(${
+    data.email
+  })
+  ## Links
+  - [Github repo](${data.repo})
+  - [Deploy Application](${data.deploy})
+  ## Screenshots
+  ![screenshot](./${data.screenshot1})
+  ![screenshot](./${data.screenshot2})
 `;
 }
 
